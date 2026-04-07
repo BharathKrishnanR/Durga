a0b9e08ea270e02afe76c919fe1dc6afbe7a42d8
BharathKrishnanR <bharathkrishnanbsc@gmail.com>
2026-04-07 23:14:53 +0530

    Updated CourseController changes

--- Course/src/main/java/com/example/Course/Controller/CourseController.java
+++ Course/src/main/java/com/example/Course/Controller/CourseController.java
@@ -28,6 +28,5 @@ public class CourseController {
 
     @GetMapping("/{id}")
     public Course getById(@PathVariable Long id) {
-        return courseService.getById(id);
-    }
+        return courseService.getById(id);}
 }
